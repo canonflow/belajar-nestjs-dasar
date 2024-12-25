544486eb09da87b0dc9cdd6e2c726700519af13b
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-12-25 23:00:11 +0700

    HTTP Response

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,8 +1,38 @@
-import { Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
-import { Request } from 'express';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpRedirectResponse,
+  Param,
+  Post,
+  Query,
+  Redirect,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('/api/users')
 export class UserController {
+  @Get('/sample-response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    return {
+      data: 'Hello World!',
+    };
+  }
+
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return {
+      url: '/api/users/sample-response',
+      statusCode: 301,
+    };
+  }
+
   @Get('/hello')
   sayHello(
     @Query('first_name') firstName: string,

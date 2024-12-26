6089478d138b001e3e281ed17689ff8cd35e5bbf
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-12-26 14:55:39 +0700

    Section View

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieparser from 'cookie-parser';
+import * as mustache from 'mustache-express';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Setting Cookie Parser
   app.use(cookieparser('RAHASIA-HAHA'));
+
+  // Setting View
+  app.set('views', __dirname + '/../views');
+  app.set('view engine', 'html');
+  app.engine('html', mustache());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

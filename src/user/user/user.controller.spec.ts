a781fe75dce66e5f25dcc2a4b5f305070526ecc8
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-12-26 17:07:30 +0700

    Section Unit Test

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import * as httpMock from 'node-mocks-http';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -12,7 +13,19 @@ describe('UserController', () => {
     controller = module.get<UserController>(UserController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should can say hello', async () => {
+    const response = await controller.sayHello('Nathan', 'Garzya');
+    expect(response).toBe('Hello Nathan Garzya');
+  });
+
+  it('should can get view', async () => {
+    const responses = httpMock.createResponse();
+    controller.viewHello('Nathan', responses);
+
+    expect(responses._getRenderView()).toBe('index.html');
+    expect(responses._getRenderData()).toEqual({
+      name: 'Nathan',
+      title: 'Template Engine',
+    });
   });
 });
